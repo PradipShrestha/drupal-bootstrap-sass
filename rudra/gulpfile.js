0b5d6c3912b14485c6471c4b1093aa5f36c14996
Pradip Shrestha (CIsSharp) <pr.pradip@gmail.com>
2016-05-12 10:45:33 -0400

    Used better way for import. Updated logo. Supported single file output

--- rudra/gulpfile.js
+++ rudra/gulpfile.js
@@ -1,17 +1,22 @@
 /**
  * Created by Pradip on 5/11/2016.
  */
+
+// Configuration setting. Change as per needed.
 var BUILD = false;
 var DEV = false;
+var SINGLE_FILE_OUTPUT = false;
+// Ends configuration setting
 
-if(BUILD && DEV){
+if (BUILD && DEV) {
     throw "Cannot have different environment true at same time";
 }
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    uglifycss = require('gulp-uglifycss');
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename');
 
 var config = {
     sassPath: './sass',
@@ -21,20 +26,28 @@ var config = {
 var sassOption = {
     errLogToConsole: true,
     // Default environment is dev
-    outputStyle: BUILD ? 'compressed': 'expanded',
-    loadPath: [config.sassPath, './bootstrap/assets/stylesheets']
+    outputStyle: BUILD ? 'compressed' : 'expanded',
+    includePaths: [config.sassPath, './bootstrap/assets/stylesheets']
 };
 
 gulp.task('sass', function () {
+    if(SINGLE_FILE_OUTPUT){
+        return gulp.src(config.sassPath + '/**/*.scss')
+            .pipe(concat('style.css'))
+            .pipe(rename({
+                basename : 'style',
+                extname : '.min.css'
+            }))
+            .pipe(sourcemaps.init())
+            .pipe(sass(sassOption).on('error', sass.logError))
+            .pipe(sourcemaps.write('./maps'))
+            .pipe(gulp.dest('./css'))
+    }
     return gulp.src(config.sassPath + '/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass(sassOption).on('error', sass.logError))
         .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./css'))
-        .pipe(uglifycss({
-            "maxLineLen": 80,
-            "uglyComments": true
-        }));
 });
 
 gulp.task('sass:watch', function () {
---
+++ rudra/sass/README.md
@@ -0,0 +1,2 @@
+* Include as many scss files you want to.
+* Use @import to include resources.
